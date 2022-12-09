df69e2281d04b6670a41cb44d59be049da695e42
Iman Anooshehpour <imananoosheh@gmail.com>
2022-12-09 12:52:55 -0800

    Added Proxy pattern in Structural section

---
+++ Structural-Proxy.ts
@@ -0,0 +1,19 @@
+/*
+Usually the best practice is when working with big data
+*/
+const original = { name: "jeff" };
+
+const reactive = new Proxy(original, {
+    get(target, key) {
+        console.log("Tracking: ", key);
+        return target[key];
+    },
+    set(target, key, value) {
+        console.log("updating UI...");
+        return Reflect.set(target, key, value);
+    },
+});
+
+reactive.name; // 'Tracking: name'
+
+reactive.name = "bob"; // 'updating UI...'
